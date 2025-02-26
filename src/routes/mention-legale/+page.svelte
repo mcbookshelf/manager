bb6dc2d343be44c1c60574a63f23053f7fc2fd66
theogiraudet <theo.giraudet@orange.fr>
2025-02-26 18:57:43 +0100

    Update +page.svelte
    Add mentions légales

--- src/routes/mention-legale/+page.svelte
+++ src/routes/mention-legale/+page.svelte
@@ -1,175 +1,29 @@
 <div class="rounded">
-    <h1>Mention Légale</h1>
+    <h1>Mentions Légales</h1>
 
-    <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat non
-        voluptate qui dolores reiciendis? Sed, earum explicabo doloremque,
-        consectetur ipsam fugit quaerat officiis voluptate recusandae enim nulla
-        tempora quidem minima. Nihil molestias voluptatem ullam temporibus
-        libero illo ipsum quo voluptate accusantium architecto sunt expedita,
-        asperiores quidem nemo iste tempora soluta odit! Porro incidunt ducimus
-        inventore consequatur numquam? Doloribus, officia quasi? Officiis
-        placeat ratione eaque maxime assumenda dicta labore delectus corporis,
-        cumque architecto. Sunt cumque cupiditate optio molestias dignissimos
-        nam recusandae, obcaecati veniam est hic voluptas. Neque magnam ducimus
-        iusto fugiat? Sed vel esse dignissimos quia soluta nam mollitia
-        voluptatum quasi amet earum voluptatibus debitis nostrum ipsa
-        necessitatibus praesentium dicta, veniam ad culpa animi veritatis.
-        Reiciendis soluta nemo architecto obcaecati omnis? Voluptatem possimus
-        sit adipisci nihil ipsum non perspiciatis labore obcaecati enim
-        consequuntur esse temporibus illo, facere quasi nulla, nesciunt commodi.
-        Totam qui suscipit perspiciatis. Voluptas saepe perspiciatis beatae
-        earum pariatur. Voluptatem aliquid maxime omnis modi atque sint
-        similique cum distinctio consequuntur sed debitis, aspernatur
-        perspiciatis ad amet quas est libero odio alias suscipit! Architecto
-        suscipit adipisci earum eius esse dolore. Temporibus consequatur a atque
-        quibusdam, velit magnam dicta sint eligendi, aliquid officia quisquam
-        obcaecati voluptatem beatae nihil dolores quia facere quis ipsam
-        asperiores quas in nobis? Facilis ad a provident. Autem deleniti nulla
-        dolore accusantium veniam velit! Nam doloremque beatae deserunt sint
-        quod odio qui vel dolorum accusantium, quo iure porro, numquam quia
-        expedita commodi saepe voluptate vitae! Necessitatibus, autem? Nam odio,
-        tenetur veniam quaerat praesentium velit possimus asperiores dolore ad
-        voluptatem assumenda. Iste illo magnam, asperiores consequatur nesciunt
-        alias deleniti et at, rem placeat mollitia sed necessitatibus quas vero!
-        Ad, labore? Magnam libero reprehenderit atque voluptatum neque nesciunt
-        voluptate, eveniet asperiores eaque officia! Dicta, repellat
-        necessitatibus, porro deleniti quis a molestias aperiam repudiandae
-        obcaecati pariatur nihil, possimus error ab? Nesciunt, deserunt a,
-        consequuntur quisquam officiis dolor molestiae quia aspernatur unde
-        deleniti exercitationem illo architecto dicta id sed provident
-        temporibus quae, vero officia autem. Quidem sint officiis amet nihil
-        praesentium? Minima obcaecati, illo atque animi laborum dignissimos
-        labore harum nulla iste perspiciatis totam recusandae quis quisquam
-        rerum officiis odit. Quam nulla necessitatibus nam quod, amet iste!
-        Provident saepe natus maxime. Quibusdam tempore placeat fuga quo dicta
-        rerum aspernatur ea qui nesciunt repellendus. Veritatis fugit, dicta et
-        non cupiditate ipsa quo id, repudiandae quam alias tempore repellendus.
-        Aliquid repellendus ipsum officiis. Consequuntur nam ipsum illum quaerat
-        excepturi deleniti aperiam ab maiores obcaecati, quod facere dolore rem
-        dignissimos optio totam nihil dicta esse accusantium eos! Ab, assumenda
-        tenetur quibusdam vitae eius perferendis? Cupiditate expedita illo, est,
-        omnis libero optio vel voluptate blanditiis asperiores dolores at
-        similique dolorum voluptas beatae veniam hic facere fugiat voluptatibus
-        quos et repellendus. Enim laborum quis inventore fugit? Ipsum, a cum sit
-        minus libero autem et animi voluptas nulla? Voluptatibus, corrupti quia
-        amet quidem doloremque voluptates atque rem nemo dolores aperiam.
-        Dignissimos, iste ad? Officiis repudiandae sed quod! Reiciendis
-        consectetur veritatis assumenda quia dolorem nulla impedit libero
-        cupiditate quae ex! Dolore natus deleniti impedit maiores excepturi
-        expedita odio quibusdam! Alias, commodi maxime? Aliquid quaerat eum
-        doloribus eos excepturi. Quod perferendis similique reiciendis maiores
-        dolorum necessitatibus veniam corrupti laudantium in, quidem accusamus
-        assumenda, repudiandae magni harum inventore vero aspernatur laboriosam
-        vel obcaecati nisi? Architecto error fugiat voluptates maxime possimus.
-        Minima impedit dolores sint fugiat nam rerum asperiores officiis quas
-        suscipit adipisci eos itaque, doloremque maxime ex quos vero veniam
-        fugit id perferendis, minus quae ipsum recusandae. Soluta, ipsum.
-        Delectus? Distinctio corporis sed reprehenderit neque rem corrupti ea
-        quas dicta id, commodi suscipit officiis esse repellendus. Possimus
-        repellat vitae voluptate dolorum facere. Ea, magnam optio. Dignissimos
-        amet sit molestiae dolorum? Non voluptatibus necessitatibus quis ipsam
-        minus facilis est, assumenda quod, debitis consequuntur commodi?
-        Suscipit culpa, quod fugiat iste ducimus enim odio ex ipsam in possimus
-        optio itaque nisi veritatis repudiandae. Nostrum natus fugiat dolor
-        eaque quisquam iste expedita corporis accusamus harum, possimus vero
-        placeat in, molestias recusandae, magni quibusdam voluptate! Suscipit
-        ducimus soluta laborum dignissimos esse iure maxime iusto fugiat.
-        Repellat beatae et ducimus dolorum maxime deserunt obcaecati laudantium
-        iste, nemo inventore, cumque quos consectetur facilis. Suscipit dolorem
-        quo molestiae ipsa repellat nisi nobis, sit distinctio beatae, iste, aut
-        possimus? Temporibus fuga ipsa aliquid voluptatem, deserunt a culpa
-        excepturi repellendus minus, delectus ex, deleniti dignissimos. Dicta
-        architecto nobis ut numquam voluptatem sequi! Neque ea mollitia ab
-        corrupti, recusandae libero tenetur. Exercitationem quisquam
-        accusantium, delectus obcaecati neque voluptas harum et labore dolores
-        impedit atque, aperiam quibusdam! Recusandae quos illo, voluptatum
-        reprehenderit in harum et expedita, laboriosam non mollitia eos,
-        reiciendis illum. Aliquam porro ex doloremque est at numquam! Voluptatem
-        fuga atque natus dolorem id libero! Dolorum, dolor deserunt. Quibusdam
-        explicabo, esse ipsum ducimus quos accusamus voluptatem minima aliquid
-        numquam dignissimos laboriosam. Explicabo quae consequuntur eum cum
-        delectus iure quam molestiae porro. Sequi voluptates ipsa reiciendis
-        cumque incidunt facere recusandae temporibus, consequuntur laborum
-        deserunt delectus ullam tenetur ipsam aliquid sapiente corrupti ex!
-        Doloremque tempora facere similique placeat, fugiat voluptatibus iste
-        aliquam id, vel cumque eaque. Nostrum facilis at, provident dolorem unde
-        explicabo. Dolores saepe reprehenderit voluptatibus beatae excepturi
-        alias in doloribus nisi. Quis, modi laboriosam quibusdam sunt molestias
-        tempora necessitatibus soluta dolor beatae explicabo, ullam, ad nisi!
-        Rem blanditiis aspernatur eligendi neque temporibus! Dolorum delectus
-        perferendis nemo atque obcaecati adipisci provident quae? Consectetur
-        autem, at hic expedita ex beatae quos reprehenderit laudantium cumque
-        voluptas illo ullam itaque voluptates necessitatibus mollitia placeat
-        praesentium labore id minima dignissimos molestiae nesciunt recusandae
-        nemo porro. Perferendis. At necessitatibus iste repellat unde dolor, quo
-        eius, ipsam voluptatibus consequatur omnis repellendus fuga debitis
-        architecto praesentium fugit corporis enim laboriosam quidem ipsum
-        consectetur! A natus aliquam enim laudantium illo. Id nobis inventore
-        magnam non optio! Maxime labore vitae consectetur! Porro magni pariatur
-        sint debitis excepturi nobis commodi dolorum mollitia odio dignissimos
-        similique voluptas, id corrupti eligendi aut. Provident, consectetur.
-        Cupiditate quas aperiam vero, placeat eligendi itaque? Illo, sed fuga
-        nam unde ipsa omnis praesentium enim modi vel magnam non animi ratione
-        similique autem, libero natus id. Ab, eligendi. Commodi. Deserunt
-        repudiandae voluptates rem ratione alias praesentium dolores assumenda
-        cum, dolor totam exercitationem nostrum fugiat fuga sequi eaque aliquid
-        nemo? Nobis hic dolorum et animi maxime quaerat sit dignissimos
-        molestias! In quod obcaecati, incidunt cumque ratione quos architecto
-        vero exercitationem repudiandae. Vel officia ipsam veritatis vitae?
-        Culpa unde perferendis eius praesentium sapiente aliquam at, voluptate
-        similique, natus optio, nihil asperiores. Cupiditate dolor velit maiores
-        asperiores commodi voluptas iure mollitia sit possimus exercitationem
-        autem, fugiat, aliquid quasi ea fuga temporibus architecto illo delectus
-        non incidunt. Nobis rerum quod molestias! Eveniet, deleniti? Mollitia,
-        quisquam ipsam error corrupti reiciendis asperiores ducimus maiores
-        impedit pariatur corporis, quod provident illum assumenda odio eius
-        dolor sit eveniet in, necessitatibus debitis voluptate. Sapiente eveniet
-        corrupti placeat ipsam. Quaerat provident nobis optio excepturi velit
-        vero, molestias ipsam, quibusdam harum explicabo dicta atque nam?
-        Suscipit repellendus aut, nesciunt tempore, consectetur voluptates,
-        nulla laudantium voluptatem sunt blanditiis veritatis. Illo, in. Vitae
-        autem adipisci aut atque ipsum minus repellat, possimus culpa quidem,
-        pariatur commodi officia. Cupiditate molestias consectetur vitae dicta,
-        molestiae temporibus maxime! Accusamus numquam ipsum debitis dolores
-        beatae eaque similique? Deleniti ipsam doloremque omnis libero quae
-        ipsum corrupti aut fugit unde cupiditate? Quam natus delectus beatae
-        voluptatem odio illo molestiae ratione placeat tempora. Aut sit
-        officiis, possimus modi maxime vero. Minima eos mollitia ipsa ipsum
-        commodi quisquam corrupti officiis. Quam explicabo, ab ipsam similique
-        officia eum debitis asperiores blanditiis veritatis autem quidem
-        doloribus sunt aperiam? Nihil at voluptas minima consequuntur. Tempora
-        accusantium error ab mollitia labore quae ducimus! Quo vel mollitia nisi
-        laboriosam ratione fugit facere repellat neque autem laudantium illo
-        saepe fugiat doloribus, porro impedit non eveniet? Asperiores, libero?
-        Corrupti repudiandae aut a harum et quae dignissimos quibusdam accusamus
-        ex unde. Velit ratione nulla assumenda beatae porro laudantium
-        accusamus, distinctio, optio quas dolores facere atque, illo reiciendis
-        tenetur eligendi? Dicta accusantium eum nemo assumenda ab ullam
-        voluptates iusto. Consequatur ratione laudantium maiores quia culpa
-        libero, pariatur, quam facere dolorem aut cupiditate animi atque
-        officiis illo excepturi nostrum obcaecati rerum! Corrupti soluta ratione
-        accusamus tenetur nihil, omnis a. Cum quod reprehenderit odit provident
-        dolorem, maxime eum laborum assumenda eaque consectetur tempora dicta
-        quae vitae ipsam consequuntur dolores in sed! Natus? Assumenda dolor at
-        commodi magnam tenetur placeat, nulla dignissimos odit nihil aspernatur?
-        Molestias ad eaque minima delectus quaerat aspernatur reprehenderit eum
-        fugit unde nisi, corrupti hic voluptas, quo laboriosam sequi. Eveniet
-        asperiores illo omnis, neque exercitationem pariatur optio beatae eum
-        quibusdam. Odit, facere quasi! Expedita, modi quam. Deleniti accusantium
-        aspernatur voluptate unde et accusamus a optio sit odio. Nemo,
-        consequatur. Voluptates accusamus minima, neque dignissimos quo officiis
-        reiciendis corporis nostrum natus mollitia. Repellat, ex totam. Odit
-        maiores esse nihil delectus quibusdam optio nemo? Dolorem doloremque
-        modi, facere odio a nulla. Quam accusamus, cupiditate repellat dolorem
-        sit ipsam perspiciatis quae facere modi quia a! Dolorum, a, velit
-        obcaecati rerum voluptates quasi molestias sunt fugiat voluptatem
-        architecto veritatis provident ex soluta non! Quia ducimus ipsam sequi
-        maxime soluta necessitatibus earum magni cupiditate quod eaque ipsa
-        dolores, quibusdam reprehenderit veniam explicabo veritatis, neque a
-        quisquam? Sequi itaque provident odio minima beatae. Autem,
-        reprehenderit.
-    </p>
+    <p>Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site mcbookshelf l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
+    
+    <h2>Edition du site</h2>
+    
+    <p>Le présent site, accessible à l’URL mcbookshelf.dev (le « Site »), est édité par :</p>
+    <p>L’association Altearn, enregistrée auprès de la préfecture/sous-préfecture de 75 - Préfecture de police de Paris sous le numéro W061014075, ayant son siège situé à Appartement 561, 16 rue Curial, 75019 PARIS, représentée par Vincent Foriel dûment habilité(e)</p>
+
+    <h2>Hébergement</h2>
+
+    <p>Le Site est hébergé par la société O2Switch, situé 222 Boulevard Gustave Flaubert, 63000 Clermont-Ferrand, (contact téléphonique : (+33) 4 44 44 60 40).</p>
+
+    <h2>Directeur de publication</h2>
+
+    <p>Le Directeur de la publication du Site est l’association Altearn.</p>
+
+    <h2>Nous contacter</h2>
+
+    <p>Par téléphone : +33699274663</p>
+    <p>Par email : contact@altearn.xyz</p>
+    <p>Par courrier : Appartement 561, 16 rue Curial, 75019 Paris</p>
+
+    <h2>Données personnelles</h2>
+    <p>Nous traitons certaines données personnelles, telles que votre adresse IP, votre user-agent et d'autres informations techniques similaires, dans le but de faire fonctionner correctement notre site. Le traitement de ces données est nécessaire à l'exécution du contrat d'utilisation du site, et ce, conformément à la base légale suivante : exécution du contrat. Nous ne traitons aucune autre donnée personnelle vous concernant.</p>
 </div>
 
 <style>
